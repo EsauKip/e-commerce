3ce043352d6f06cdd6517934df746eadf17f90bf
EsauKip <esau.kiprono@student.moringaschool.com>
2022-08-31 13:28:11 +0300

    add background image

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import './App.css'
+import Home from './component/Home/Home'
 import NavBar from './component/Navbar/Navbar'
 const App = () => {
   return (
     <>
     <NavBar/>
-    
+    <Home/>
     </>
   )
 }
---
+++ src/component/Home/Home.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Home.css";
+const Home = () => {
+  return (
+    <div className="hero">
+      <div class="card bg-dark text-white border-0">
+        <img src="/assets/bgg.jpeg" class="card-img" alt="background" height="550px" />
+        <div class="card-img-overlay">
+        <div className="container">
+        <h5 class="card-title display-3 text-dark">New Season's Arrival</h5>
+        <p class="card-text">
+          
+        </p>
+        </div>
+        
+       
+        </div> 
+      </div>
+    </div>
+  );
+};
+
+export default Home;
