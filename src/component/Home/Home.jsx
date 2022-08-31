18b5c6a3059f7b30fb5c0ac48e890f2a10699d59
EsauKip <esau.kiprono@student.moringaschool.com>
2022-08-31 14:21:29 +0300

    use react hook ,fetch the fakestore api

--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -3,18 +3,21 @@ import "./Home.css";
 const Home = () => {
   return (
     <div className="hero">
-      <div class="card bg-dark text-white border-0">
-        <img src="/assets/bgg.jpeg" class="card-img" alt="background" height="550px" />
-        <div class="card-img-overlay">
-        <div className="container">
-        <h5 class="card-title display-3 text-dark">New Season's Arrival</h5>
-        <p class="card-text">
-          
-        </p>
+      <div className="card bg-dark text-white border-0">
+        <img
+          src="/assets/bgg.jpeg"
+          className="card-img"
+          alt="background"
+          height="550px"
+        />
+        <div className="card-img-overlay d-flex flex-column justify-content-center">
+          <div className="container">
+            <h5 className="card-title display-3 text-dark fw-bolder mb-0">
+              New Season's Arrival
+            </h5>
+            <p className="card-text lead fs-2">CHECK OUT ALL THE TRENDS</p>
+          </div>
         </div>
-        
-       
-        </div> 
       </div>
     </div>
   );
