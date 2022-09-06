3e7464be06f03f6d43db4ce2ca6d1b9aabfd5dac
EsauKip <esau.kiprono@student.moringaschool.com>
2022-09-06 11:34:02 +0300

    style the proguct page

--- src/component/Product.jsx
+++ src/component/Product.jsx
@@ -1,6 +1,7 @@
 import React, {useState, useEffect}from 'react'
 import {useParams} from 'react-router'
 import { NavLink } from 'react-router-dom';
+import Skeleton from 'react-loading-skeleton';
 const Product = () => {
     const {id}=useParams();
     const [product,setProduct]=useState([]);
@@ -18,7 +19,18 @@ const Product = () => {
      const Loading = () => {
         return(
             <>
-            Loading.......
+            <div className="col-md-6" style={{lineHeight:2}}>
+            <Skeleton height={400}/>
+            </div>
+            <div className="col-md-6">
+            <Skeleton height={50} width={300}/>
+            <Skeleton height={75}/>
+            <Skeleton height={25} width={150}/>
+            <Skeleton height={50}/>
+            <Skeleton height={150}/>
+            <Skeleton height={50} width={100}/>
+            <Skeleton height={50} width={100} style={{marginLeft:6}}/>
+            </div>
             </>
         )
      };
